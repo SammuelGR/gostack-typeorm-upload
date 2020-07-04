689c97398fa65cbc214fb402e2484268af7eb1fa
Sammuel Reis <sammugr@hotmail.com>
2020-07-03 23:59:31 -0300

    implements a route to delete transactions
    the route delete one transaction based on its id

--- src/routes/transactions.routes.ts
+++ src/routes/transactions.routes.ts
@@ -1,12 +1,12 @@
 import { Router } from 'express';
+// import AppError from '../errors/AppError';
 
-import TransactionsRepository from '../repositories/TransactionsRepository';
+// import TransactionsRepository from '../repositories/TransactionsRepository';
 import CreateTransactionService from '../services/CreateTransactionService';
-// import DeleteTransactionService from '../services/DeleteTransactionService';
+import DeleteTransactionService from '../services/DeleteTransactionService';
 // import ImportTransactionsService from '../services/ImportTransactionsService';
 
 const transactionsRouter = Router();
-const transactionRepository = new TransactionsRepository();
 
 transactionsRouter.get('/', async (request, response) => {
   // TODO
@@ -28,7 +28,13 @@ transactionsRouter.post('/', async (request, response) => {
 });
 
 transactionsRouter.delete('/:id', async (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const deleteTransaction = new DeleteTransactionService();
+
+  await deleteTransaction.execute(id);
+
+  return response.status(204).send();
 });
 
 transactionsRouter.post('/import', async (request, response) => {
